664a163b9bbde3fa0991f5f6472ceedc91346453
zhangzongzheng <zzzhaichao@gmail.com>
2017-12-19 23:11:31 +0800

    update route

--- routes/article.js
+++ routes/article.js
@@ -2,12 +2,12 @@ const router = require('koa-router')()
 const controller = require('../controller')
 
 // 创建文章
-router.post('/article', controller.article.create)
+router.post('/articles', controller.article.create)
 
 // 删除文章
-router.del('/article/:articleId', controller.article.remove)
+router.del('/articles/:articleId', controller.article.remove)
 
 // 获取文章列表
-router.get('/article', controller.article.list)
+router.get('/articles', controller.article.list)
 
 module.exports = router
